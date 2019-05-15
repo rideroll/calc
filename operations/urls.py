e350350f8aa270bd7192962fcfeb9cc25915d95a
rideroll <sashuid@gmail.com>
2019-05-15 22:37:48 +0300

    adding subtract

--- operations/urls.py
+++ operations/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('add/<int:arg_1>/<int:arg_2>/', views.add, name='add')
+    path('add/<int:arg_1>/<int:arg_2>/', views.add, name='add'),
+    path('subtract/<int:arg_1>/<int:arg_2>/', views.subtract, name='subtract')
 ]
